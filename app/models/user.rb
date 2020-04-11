5019a717ec8ec014aac9b63bd85dd86202c45264
eugene <evgeni.valentino@gmail.com>
2020-04-12 02:48:35 +0300

    Correct previous mistakes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :users_passed_tests
 
   def passed_tests(level)
-    UsersPassedTest.where( "user_id = ?", self.id ).joins(:test).where("tests.level = ?", level )
+    tests.where(level: level)
   end
 end
