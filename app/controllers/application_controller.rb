a5e2a4adc25249a312748247925a9bebfa7dfcbe
eugene <evgeni.valentino@gmail.com>
2020-05-02 05:54:43 +0300

    Add fields (first_name, last_name) to registration view. Add flash message after sign_up/in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
+    flash[:notice] = "Hello, #{user.first_name}!"
     user.is_a?(Admin) ? admin_tests_path : root_path
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :email, :password, :password_confirmation])
   end
 end
