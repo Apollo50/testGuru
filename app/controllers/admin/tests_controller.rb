0087c6b11e678668db4f79e345a35ed9e4f6c439
eugene <evgeni.valentino@gmail.com>
2020-05-02 04:48:30 +0300

    Adding Admin like a test author after create new test.

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :find_test, only: %i[start show edit update destroy]
+  before_action :find_test, only: %i[show edit update destroy]
 
   def index
     @tests = Test.all
@@ -16,10 +16,10 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def create
-    @test = Test.new
+    @test = current_user.author_tests.new(test_params)
 
     if @test.save
-      redirect_to @test
+      redirect_to admin_tests_path
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::TestsController < Admin::BaseController
 
   def update
     if @test.update(test_params)
-      redirect_to @test
+      redirect_to admin_tests_path
     else
       render :edit
     end
@@ -35,12 +35,7 @@ class Admin::TestsController < Admin::BaseController
 
   def destroy
     @test.destroy
-    redirect_to tests_path
-  end
-
-  def start
-    current_user.tests.push(@test)
-    redirect_to current_user.passage_test(@test)
+    redirect_to admin_tests_path
   end
 
   private
