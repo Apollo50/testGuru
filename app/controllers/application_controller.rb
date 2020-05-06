4b37fd9c6aa968659021df0ed77f62edef657567
eugene <evgeni.valentino@gmail.com>
2020-05-06 03:38:00 +0300

    Localize resources (tests, questions, answers) for admin & user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   def after_sign_in_path_for(user)
     flash[:notice] = "Hello, #{user.first_name}!"
@@ -10,5 +15,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :email, :password, :password_confirmation])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
