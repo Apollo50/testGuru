82f2522091d30c6856f58d483ce6cbe802ffc015
eugene <evgeni.valentino@gmail.com>
2020-05-06 05:07:15 +0300

    Add feature: hide current local if it's default local

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def default_url_options
-    { lang: I18n.locale }
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
   end
 
   def after_sign_in_path_for(user)
