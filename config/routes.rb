ca4677e497fdeb373ec2f771bcc6d6b7a3b41a7a
eugene <evgeni.valentino@gmail.com>
2020-05-08 16:59:33 +0300

    Add link  admin_root_path to navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'tests#index'
+  get '/admin/tests', to: 'admin/tests#index', as: :admin_root
 
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
@@ -21,7 +22,7 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :tests do
+    resources :tests, except: :index do
       resources :questions, shallow: true, except: :index do
         resources :answers, shallow: true, except: :index
       end
